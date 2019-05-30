451c604397c547ac4e665ed3e0f42efafd42e92a
Noah Scholfield <contact@noahscholfield.com>
2019-05-30 15:48:16 -0400

    Delete helper comments

--- lib/main.dart
+++ lib/main.dart
@@ -19,15 +19,6 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Conditions',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primaryColor: Colors.grey[800],
         textTheme: TextTheme(
           title: TextStyle(
@@ -45,15 +36,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -107,21 +89,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: ListView(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         children: <Widget>[
           new Container(
             margin: EdgeInsets.all(10.0),
@@ -147,14 +119,14 @@ class _MyHomePageState extends State<MyHomePage> {
               if (snapshot.hasData) {
                 return new Column(
                   children: <Widget>[
-                    // Sun Card
                     new SunCard(snapshot.data),
-                    // Moon Card
                     new MoonCard(snapshot.data),
                   ]
                 );
               } else {
-                return new Text('Loading...');
+                return new Center(
+                  child: new Text('Loading...', style: Theme.of(context).textTheme.headline)
+                );
               }
             }
           )
