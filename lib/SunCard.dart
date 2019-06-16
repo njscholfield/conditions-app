e4c548f5c9d16c35b8282e1a5d3f5a74df8bb36d
Noah Scholfield <contact@noahscholfield.com>
2019-06-15 20:12:39 -0400

    Open sub page when widget tapped

--- lib/SunCard.dart
+++ lib/SunCard.dart
@@ -41,80 +41,94 @@ class SunCardState extends State<SunCard> with SingleTickerProviderStateMixin {
     super.dispose();
   }
 
-
-@override
+  @override
   Widget build(BuildContext context) {
-    return new Container(
-      decoration: new BoxDecoration(color: Colors.green[300]),
-      margin: EdgeInsets.all(10.0),
-      padding: EdgeInsets.all(20.0),
-      child: Column(
-        children: <Widget>[
-          new Text('Sun', style: Theme.of(context).textTheme.display1),
-          new Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              new Column(
-                children: <Widget>[
-                  new Padding(
-                    padding: EdgeInsets.only(bottom: 12.0),
-                    child: new Row(
-                      children: <Widget>[
-                        new Icon(
-                          FontAwesomeIcons.solidSun,
-                          color: Colors.yellow[600],
-                          size: 40
-                        ),
-                        new Icon(FontAwesomeIcons.longArrowAltUp),
-                      ]
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(
+          builder: (context) {
+            return new Scaffold(
+              appBar: AppBar(
+                title: Text('Sun Details'),
+              ),
+              body: SunCard(widget.astronData)
+            );
+          },
+        ));
+      },
+      child: new Container(
+        decoration: new BoxDecoration(color: Colors.green[300]),
+        margin: EdgeInsets.all(10.0),
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text('Sun', style: Theme.of(context).textTheme.display1),
+            new Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: <Widget>[
+                new Column(
+                  children: <Widget>[
+                    new Padding(
+                      padding: EdgeInsets.only(bottom: 12.0),
+                      child: new Row(
+                        children: <Widget>[
+                          new Icon(
+                            FontAwesomeIcons.solidSun,
+                            color: Colors.yellow[600],
+                            size: 40
+                          ),
+                          new Icon(FontAwesomeIcons.longArrowAltUp),
+                        ]
+                      ),
                     ),
-                  ),
-                  new Text(
-                    widget.astronData.sunrise,
-                    style: Theme.of(context).textTheme.title.copyWith(color: _animation.value),
-                  ),
-                ]
-              ),
-              new Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  new Padding(
-                    padding: EdgeInsets.only(bottom: 12.0),
-                    child: new Row(
-                      children: <Widget>[
-                        new Icon(
-                          FontAwesomeIcons.solidSun,
-                          color: Colors.yellow[800],
-                          size: 40,
-                        ),
-                        new Icon(FontAwesomeIcons.longArrowAltDown),
-                      ],
+                    new Text(
+                      widget.astronData.sunrise,
+                      style: Theme.of(context).textTheme.title.copyWith(color: _animation.value),
                     ),
-                  ),
-                  new Text(
-                    widget.astronData.sunset,
-                    style: Theme.of(context).textTheme.title.copyWith(color: _animation.value),
-                  ),
-                ],
-              ),
-              new Column(
-                children: <Widget>[
-                  new Padding(
-                    padding: EdgeInsets.only(bottom: 12.0),
-                    child: new Icon(
-                      FontAwesomeIcons.solidMoon,
-                      color: Colors.blueGrey[600],
-                      size: 40
-                    )
-                  ),
-                  new Text(
-                    widget.astronData.dusk,
-                    style: Theme.of(context).textTheme.title.copyWith(color: _animation.value),
-                  ),
-                ]
-              ),
-          ]),
-        ],
+                  ]
+                ),
+                new Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    new Padding(
+                      padding: EdgeInsets.only(bottom: 12.0),
+                      child: new Row(
+                        children: <Widget>[
+                          new Icon(
+                            FontAwesomeIcons.solidSun,
+                            color: Colors.yellow[800],
+                            size: 40,
+                          ),
+                          new Icon(FontAwesomeIcons.longArrowAltDown),
+                        ],
+                      ),
+                    ),
+                    new Text(
+                      widget.astronData.sunset,
+                      style: Theme.of(context).textTheme.title.copyWith(color: _animation.value),
+                    ),
+                  ],
+                ),
+                new Column(
+                  children: <Widget>[
+                    new Padding(
+                      padding: EdgeInsets.only(bottom: 12.0),
+                      child: new Icon(
+                        FontAwesomeIcons.solidMoon,
+                        color: Colors.blueGrey[600],
+                        size: 40
+                      )
+                    ),
+                    new Text(
+                      widget.astronData.dusk,
+                      style: Theme.of(context).textTheme.title.copyWith(color: _animation.value),
+                    ),
+                  ]
+                ),
+            ]),
+          ],
+        ),
       ),
     );
   }
